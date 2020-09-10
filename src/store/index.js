4b61419e2ceb6d42abe1540f015f8b8bbb638880
higuzero <higuzeroggw@gmail.com>
2020-09-10 22:53:00 +0900

    [mod] user の state を保持できるように変更

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    user: null
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
